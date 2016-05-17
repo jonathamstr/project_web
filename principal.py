ac18735a8f2dc851d1c56b28894753da84d7e50b
Jonathan Aguirre <jonathanaguirreglez@gmail.com>
2016-05-17 13:15:41 +0200

    SDFASD
    asdf

--- principal.py
+++ principal.py
@@ -1,20 +1,47 @@
 from flask import *
+from sqlalchemy import *
+from sqlalchemy.sql import *
 app = Flask(__name__)
 app.secret_key = 'iswuygdedgv{&75619892__01;;>..zzqwQIHQIWS'
+engine = create_engine('sqlite:///mabase.db', echo=True)
+metadata = MetaData()
+
+users = Table('users', metadata,
+    Column('cle_util', Integer, autoincrement=True, primary_key=True),
+    Column('email_util', String),
+    Column('nom_util', String),
+    Column('motpass', String),
+    Column('info_uti',String))
 
 @app.route('/')
 def index():
     logged = 'logged' in session
     message = ""
     if logged:
-        message = session['name']
+        message = session['Email'] + session['Password']
     #return redirect(url_for('static', filename='html/index.html'))
     return render_template('html/index.html',message=message, logged=logged)
 
 @app.route('/login',methods=['POST'])
 def login():
-    session['name'] = escape(request.form['Email'])
-    session['logged'] = True
+    session['Email'] = escape(request.form['Email'])
+    session['Password'] = escape(request.form['Password'])
+    metadata.create_all(engine)
+    connection = engine.connect()
+    s = select([users.c.nom_util]).\
+        where(
+            and_(
+                users.c.nom_util.like(session['Email']),
+                users.c.motpass.like(session['Password'])
+            )
+        )
+    for row in connection.execute(s):
+        print(row)
+        session['logged'] = True
+        print("User found")
+    print('\n')
+    connection.close()
+
     return redirect('/')
 
 @app.route('/logout')
